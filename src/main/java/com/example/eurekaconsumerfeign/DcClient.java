acb1b466232420f368f1331cb77f757b0404be91
heruihong0325 <18655377896@163.com>
2019-12-17 15:44:01 +0800

    新增断路器和rabbitmq监听

--- src/main/java/com/example/eurekaconsumerfeign/DcClient.java
+++ src/main/java/com/example/eurekaconsumerfeign/DcClient.java
@@ -1,12 +1,21 @@
 package com.example.eurekaconsumerfeign;
 
 import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient("eureka-client")
+@FeignClient(name = "eureka-client",fallback =UserFeignClientFallback.class )
 public interface DcClient {
 
     @GetMapping("/dc")
     String consumer();
 
+}
+@Component
+ class UserFeignClientFallback implements DcClient {
+
+    @Override
+    public String consumer() {
+        return "fallback";
+    }
 }
--- src/main/java/com/example/eurekaconsumerfeign/EurekaConsumerFeignApplication.java
+++ src/main/java/com/example/eurekaconsumerfeign/EurekaConsumerFeignApplication.java
@@ -2,6 +2,7 @@ package com.example.eurekaconsumerfeign;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
@@ -9,6 +10,7 @@ import org.springframework.cloud.netflix.feign.EnableFeignClients;
  * @author Administrator
  */
 @EnableFeignClients
+@EnableCircuitBreaker
 @EnableDiscoveryClient
 @SpringBootApplication
 public class EurekaConsumerFeignApplication {
