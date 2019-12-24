96e2b08a290751ba9dc2e4a745906031220e6305
heruihong0325 <18655377896@163.com>
2019-12-24 20:56:05 +0800

    新增服务跟踪

--- src/main/java/com/example/eurekaconsumerfeign/controller/DcController.java
+++ src/main/java/com/example/eurekaconsumerfeign/controller/DcController.java
@@ -1,6 +1,8 @@
 package com.example.eurekaconsumerfeign.controller;
 
 import com.example.eurekaconsumerfeign.DcClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,12 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DcController {
 
+    private static Logger logger= LoggerFactory.getLogger(DcController.class);
     @Autowired
     DcClient dcClient;
 
 
     @GetMapping("/consumer")
     public String dc() {
+        logger.info("请求fegin");
         return dcClient.consumer();
     }
 
